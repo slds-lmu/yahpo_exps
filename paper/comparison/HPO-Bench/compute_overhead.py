eb7efd076508ff8474ed0449594478504d49e7d6
pfistfl <pfistererf@googlemail.com>
2022-02-21 12:00:00 +0100

    add overhead

---
+++ paper/comparison/HPO-Bench/compute_overhead.py
@@ -0,0 +1,33 @@
+import yahpo_gym
+
+
+import numpy as np
+import time
+import os, psutil
+def eval_time(objfun, repls = 1):
+    t = [None for _ in range(repls)]
+    for i in range(repls):
+        t0 = time.time(); 
+        objfun()
+        t1 = time.time()
+        t[i] = t1 - t0
+    
+    t = np.array([t]).mean()
+    print(f'Time taken: {np.round(t, 3)} s')
+    print(f'Memory: {psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2} MB')
+
+def objfun():
+    n_trials = 50
+    bench = yahpo_gym.BenchmarkSet("lcbench", instance = '3945')
+    bench.set_instance("3945")
+    xs = [None for _ in range(n_trials)]
+    # Random search
+    for i in range(n_trials):
+        # Sample a point from the configspace and evaluate
+        value = bench.config_space.sample_configuration(1).get_dictionary()
+        xs[i] = bench.objective_function(value)
+    return xs
+
+
+if __name__ == '__main__':
+    eval_time(objfun)
